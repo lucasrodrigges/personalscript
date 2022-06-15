9b898773744ad32d64a4b5d3629b8e6af6c5d088
Lucas Rodrigues <rodrigges@Lucass-iMac.local>
2022-06-15 00:56:24 -0300

    updating macscript

--- mac-script.sh
+++ mac-script.sh
@@ -5,8 +5,8 @@ defaults write -g ApplePressAndHoldEnabled -bool false
 ## Brew Apps ##
 brew install --cask google-chrome
 brew install --cask firefox
-brew install --cask whatsapp
-brew install --cask telegram
+# brew install --cask whatsapp
+# brew install --cask telegram
 brew install --cask discord
 brew install --cask slack
 brew install --cask zoom
@@ -16,18 +16,20 @@ brew install --cask hackintool
 brew install git
 brew install --cask android-studio
 brew install --cask anydesk
-brew install --cask balenaetcher
+# brew install --cask balenaetcher
 brew install --cask flameshot
 brew install --cask blackhole-2ch
 brew install --cask obs
-brew install --cask steam
-brew install --cask qbittorrent
+# brew install --cask steam
+# brew install --cask qbittorrent
 brew install --cask reaper
 brew install --cask google-drive
 brew install --cask stats
-brew install --cask vlc
-brew install --cask spotify
+# brew install --cask vlc
+# brew install --cask spotify
 brew install --cask rectangle
-brew install --cask geekbench
+# brew install --cask geekbench
 brew install --cask android-file-transfer
-brew install --cask notion
+# brew install --cask notion
+brew install zsh
+bew install curl
