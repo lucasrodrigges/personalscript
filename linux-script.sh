12a2a08803f4f817a51e8e29915fb44e4d3d28d9
Lucas Rodrigues <99984705+lucasrodrigges@users.noreply.github.com>
2022-07-29 12:10:43 -0300

    Update linux-script.sh

--- linux-script.sh
+++ linux-script.sh
@@ -14,34 +14,34 @@ cd /home/$USER/Downloads/Programas
 wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
 
 ## Installing Slack (.deb)
-wget -c https://downloads.slack-edge.com/releases/linux/4.26.1/prod/x64/slack-desktop-4.26.1-amd64.deb 
+# wget -c https://downloads.slack-edge.com/releases/linux/4.26.1/prod/x64/slack-desktop-4.26.1-amd64.deb 
 
 ## Installing Zoom
-wget -c https://cdn.zoom.us/prod/5.10.7.3311/zoom_amd64.deb
-sudo dpkg -i *deb
+# wget -c https://cdn.zoom.us/prod/5.10.7.3311/zoom_amd64.deb
+# sudo dpkg -i *deb
 
 ## Installing VSCode ##
-wget -c https://az764295.vo.msecnd.net/stable/30d9c6cd9483b2cc586687151bcbcd635f373630/code_1.68.1-1655263094_amd64.deb && sudo dpkg -i *deb
+# wget -c https://az764295.vo.msecnd.net/stable/30d9c6cd9483b2cc586687151bcbcd635f373630/code_1.68.1-1655263094_amd64.deb && sudo dpkg -i *deb
 
 #Installing Jack ##
-sudo apt install qjackctl -y
+# sudo apt install qjackctl -y
 
 ## Apt Apps ##
 sudo apt install flameshot -y
 sudo apt install kcolorchooser -y
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt-get install apt-transport-https -y
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update -y
-sudo apt-get install sublime-text -y
+# wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+# sudo apt-get install apt-transport-https -y
+# echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+# sudo apt-get update -y
+# sudo apt-get install sublime-text -y
 
 ## Repository Softwares ##
-# sudo apt install flatpak -y
-# sudo add-apt-repository ppa:flatpak/stable -y
-# sudo apt update -y
-# sudo apt install flatpak -y
-# sudo apt install gnome-software-plugin-flatpak -y 
-# flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo 
+sudo apt install flatpak -y
+sudo add-apt-repository ppa:flatpak/stable -y
+sudo apt update -y
+sudo apt install flatpak -y
+sudo apt install gnome-software-plugin-flatpak -y 
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo 
 
 ## Apps Flatpak ##
 flatpak install flathub org.videolan.VLC -y
