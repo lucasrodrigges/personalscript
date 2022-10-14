53eacfea1eacce08849a2975107fbcfe85aa2a3a
Lucas Rodrigues <rodrigges@pop-os.localdomain>
2022-10-14 15:04:57 -0300

    updating mac-script

--- mac-script.sh
+++ mac-script.sh
@@ -2,34 +2,45 @@
 ## Enabling Key Repeat ##
 defaults write -g ApplePressAndHoldEnabled -bool false
 
+## Installing brew ##
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 ## Brew Apps ##
+brew install zsh
+bew install curl
+brew install git
+brew install docker
+brew install docker-compose
+brew install mysql
+brew install --cask mysqlworkbench
+brew install --cask dbeaver-community
+brew install --cask visual-studio-code
+brew install --cask android-studio
+# brew install --cask sublime-text
+
+brew install --cask stats
+brew install --cask rectangle
+brew install --cask blackhole-2ch
+brew install --cask android-file-transfer
+
+
 brew install --cask google-chrome
 brew install --cask firefox
-# brew install --cask whatsapp
-# brew install --cask telegram
+brew install --cask whatsapp
+brew install --cask telegram
 brew install --cask discord
 brew install --cask slack
 brew install --cask zoom
-brew install --cask sublime-text
-brew install --cask visual-studio-code
 brew install --cask hackintool
-brew install git
-brew install --cask android-studio
-brew install --cask anydesk
-# brew install --cask balenaetcher
-brew install --cask flameshot
-brew install --cask blackhole-2ch
 brew install --cask obs
-# brew install --cask steam
-# brew install --cask qbittorrent
 brew install --cask reaper
 brew install --cask google-drive
-brew install --cask stats
+brew install --cask qbittorrent
+brew install --cask notion
+# brew install --cask anydesk
+# brew install --cask balenaetcher
+# brew install --cask flameshot
+# brew install --cask steam
 # brew install --cask vlc
 # brew install --cask spotify
-brew install --cask rectangle
 # brew install --cask geekbench
-brew install --cask android-file-transfer
-# brew install --cask notion
-brew install zsh
-bew install curl
