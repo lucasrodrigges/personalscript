233db0c0b053aaffc4ff5c5c08be9be7de1d7924
Lucas Rodrigues <99984705+lucasrodrigges@users.noreply.github.com>
2022-10-19 13:14:13 -0300

    Update linux-script.sh

--- linux-script.sh
+++ linux-script.sh
@@ -14,17 +14,18 @@ cd /home/$USER/Downloads/Programas
 wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
 
 ## Installing Slack (.deb)
-# wget -c https://downloads.slack-edge.com/releases/linux/4.26.1/prod/x64/slack-desktop-4.26.1-amd64.deb 
+sudo snap install slack
 
 ## Installing Zoom
-# wget -c https://cdn.zoom.us/prod/5.10.7.3311/zoom_amd64.deb
-# sudo dpkg -i *deb
+wget -c https://cdn.zoom.us/prod/5.12.2.4816/zoom_amd64.deb
 
 ## Installing VSCode ##
-# wget -c https://az764295.vo.msecnd.net/stable/30d9c6cd9483b2cc586687151bcbcd635f373630/code_1.68.1-1655263094_amd64.deb && sudo dpkg -i *deb
+wget -c https://az764295.vo.msecnd.net/stable/d045a5eda657f4d7b676dedbfa7aab8207f8a075/code_1.72.2-1665614327_amd64.deb
+
+sudo dpkg -i *deb
 
 #Installing Jack ##
-# sudo apt install qjackctl -y
+sudo apt install qjackctl -y
 
 ## Apt Apps ##
 sudo apt install flameshot -y
@@ -46,7 +47,7 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 ## Apps Flatpak ##
 flatpak install flathub org.videolan.VLC -y
 flatpak install flathub com.discordapp.Discord -y
-flatpak install flathub io.github.shiftey.Desktop
+# flatpak install flathub io.github.shiftey.Desktop
 flatpak install flathub com.obsproject.Studio -y
 flatpak install flathub com.leinardi.gst -y
 flatpak install flathub com.anydesk.Anydesk -y
@@ -54,7 +55,7 @@ flatpak install flathub com.anydesk.Anydesk -y
 ## Customizing UBUNTU ##
 sudo apt-get install chrome-gnome-shell -y
 sudo apt install gnome-shell-extensions -y
-sudo apt install menulibre -y
+# sudo apt install menulibre -y
 
 
 echo "All done! Reboot your system."
