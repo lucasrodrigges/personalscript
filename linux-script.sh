4e77b2560b2b96c94291db9304cd848bd6f254cd
lucasrodrigges <rodriggeslucas@gmail.com>
2023-05-09 14:46:33 -0300

    fix linux-script

--- linux-script.sh
+++ linux-script.sh
@@ -1,53 +1,42 @@
+#!/bin/bash
+
 # Captura o sinal da tecla "Esc" e interrompe o script
 trap 'echo "Script interrompido pelo usuário." && exit 1' INT TERM EXIT
 
 # Exibe uma mensagem informando sobre a opção de interromper o script
 echo "Pressione a tecla 'Esc' a qualquer momento para cancelar o script."
 
-#!/usr/bin/env bash
-
-# Set user variable
+# Seta a variável de usuário
 USER_NAME="$USER"
 
-# Exit immediately if a command exits with a non-zero status
+# Cancela imeadiatamente se um comando sair com um status não zero
 set -e
 
 # Remove occasional crashes from apt
 sudo rm -f /var/lib/dpkg/lock-frontend
 sudo rm -f /var/cache/apt/archives/lock
 
-# Add Google Chrome repository key
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+# Instalando Dialog para criar caixas de diálogo
+sudo apt-get update
+sudo apt-get install -y dialog
 
-# Add Google Chrome repository to system
-echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+# Defina a lista de softwares
+SOFTWARES="Google Chrome off
+Flameshot off"
 
-# Update apt package list
-sudo apt update
+# Crie uma caixa de diálogo com os programas para o usuário escolher
+CHOICES=$(dialog --stdout --checklist 'Selecione os softwares que você deseja instalar:' 0 0 0 $SOFTWARES)
 
-# Install Google Chrome
-sudo apt install -y google-chrome-stable
+# Instale os softwares selecionados
+for CHOICE in $CHOICES; do
+    case $CHOICE in
+        "Google Chrome") wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add - && echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list && sudo apt update && sudo apt install -y google-chrome-stable ;;
+        "Flameshot") sudo apt install -y flameshot ;;
+    esac
+done
 
-# Install Jack
-sudo apt install -y qjackctl
-
-# Install apt applications
-sudo apt install -y flameshot kcolorchooser
-
-# Install Flatpak (if not installed)
-if ! command -v flatpak &> /dev/null; then
-    sudo apt install -y flatpak
+# Reinicie o sistema para aplicar as mudanças
+if dialog --yesno 'A instalação foi concluída. Você deseja reiniciar o sistema agora?' 0 0; then
+    sudo reboot
 fi
-
-# Add Flatpak repository and update package list
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-sudo apt update
-
-# Install Flatpak applications
-flatpak install -y flathub com.visualstudio.code com.getpostman.Postman rest.insomnia.Insomnia io.dbeaver.DBeaverCommunity us.zoom.Zoom com.discordapp.Discord com.slack.Slack io.github.shiftey.Desktop com.obsproject.Studio com.leinardi.gst org.videolan.VLC
-
-# Install Chrome Gnome Shell and Gnome Shell Extensions
-sudo apt install -y chrome-gnome-shell gnome-shell-extensions
-
-# Prompt user to reboot system
-echo "All done! Please reboot your system to apply changes."
+```
