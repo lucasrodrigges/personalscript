b66af350b9d03bcbf4bcdc28bb1659aee393005b
lucasrodrigges <rodriggeslucas@gmail.com>
2023-05-09 16:34:32 -0300

    add nvm

--- linux-script.sh
+++ linux-script.sh
@@ -20,6 +20,18 @@ sudo rm -f /var/cache/apt/archives/lock
 sudo apt-get update
 sudo apt-get install -y dialog
 
+if ! grep -q "flathub" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+    if dialog --yesno '
+        O repositório flatpak não está instalado. Você deseja instalar agora?
+        Não instalá-lo pode causar problemas na instalação de alguns softwares.
+    ' 0 0; then
+        sudo apt install flatpak
+        sudo apt install gnome-software-plugin-flatpak
+        flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+        sudo apt update
+    fi
+fi
+
 # Defina a lista de softwares
 SOFTWARES=(
   "Google Chrome" "" OFF \
@@ -27,18 +39,46 @@ SOFTWARES=(
   "Kcolo Chooser" "" OFF \
 )
 
+DEV_SOFTWARES=(
+  "curl" "" OFF \
+  "git" "" OFF \
+  "wget" "" OFF \
+  "npm" "" OFF \
+  "nvm (requer curl instalado)" "" OFF \
+)
+
 # Crie uma caixa de diálogo com os programas para o usuário escolher
 CHOICES=$(dialog --stdout --checklist 'Selecione os softwares que você deseja instalar:' 0 0 0 "${SOFTWARES[@]}")
 
+# Crie uma caixa de diálogo com os programas de desenvolvimento para o usuário escolher
+DEV_CHOICES=$(dialog --stdout --checklist 'Selecione os softwares de desenvolvimento que você deseja instalar:' 0 0 0 "${DEV_SOFTWARES[@]}")
 
 # Instale os softwares selecionados
 for CHOICE in $CHOICES; do
     case $CHOICE in
+        "Google Chrome") wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo apt install -y ./google-chrome-stable_current_amd64.deb ;;
         "KcoloChooser") sudo apt install -y kcolorchooser ;;
         "Flameshot") sudo apt install -y flameshot ;;
-        "curl") sudo apt install -y curl ;;
     esac
 done
+
+# Instale os softwares de desenvolvimento selecionados
+for DEV_CHOICE in $DEV_CHOICES; do
+    case $DEV_CHOICE in
+        "curl") sudo apt install -y curl ;;
+        "git") sudo apt install -y git ;;
+        "wget") sudo apt install -y wget ;;
+        "npm") sudo apt install -y npm ;;
+        "nvm") curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash ;;
+    esac
+done
+
+#  Reiniciar o bash e verificar se o nvm foi instalado
+if which nvm >/dev/null; then
+    echo "O NVM foi instalado corretamente."
+else
+    echo "O NVM não foi instalado."
+fi
 
 # Reinicie o sistema para aplicar as mudanças
 if dialog --yesno 'A instalação foi concluída. Você deseja reiniciar o sistema agora?' 0 0; then
