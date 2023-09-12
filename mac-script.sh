8adaec96ec2472f90b1c60bcf8506a45b5fd0c0e
Lucas Rodrigues <99984705+lucasrodrigges@users.noreply.github.com>
2023-09-12 11:35:40 -0300

    update mac-script

--- mac-script.sh
+++ mac-script.sh
@@ -11,16 +11,16 @@ bew install curl
 brew install git
 # brew install mysql
 brew install --cask dbeaver-community
-brew install --cask mysqlworkbench
+# brew install --cask mysqlworkbench
 # brew install --cask dbeaver-community
 brew install --cask visual-studio-code
-brew install --cask android-studio
-brew install --cask sublime-text
+# brew install --cask android-studio
+# brew install --cask sublime-text
 brew install --cask postman
 
 brew install --cask stats
 brew install --cask rectangle
-brew install --cask blackhole-2ch
+# brew install --cask blackhole-2ch
 brew install --cask android-file-transfer
 
 
@@ -29,19 +29,19 @@ brew install --cask firefox
 brew install --cask whatsapp
 brew install --cask telegram
 brew install --cask discord
-brew install --cask slack
-brew install --cask zoom
+# brew install --cask slack
+# brew install --cask zoom
 brew install --cask hackintool
 brew install --cask obs
 brew install --cask reaper
 brew install --cask google-drive
 brew install --cask qbittorrent
-brew install --cask notion
-brew install --cask anydesk
-brew install --cask balenaetcher
-brew install --cask flameshot
+# brew install --cask notion
+# brew install --cask anydesk
+# brew install --cask balenaetcher
+# brew install --cask flameshot
 brew install --cask spotify
-brew install gimp
+# brew install gimp
 brew install musescore
 # brew install --cask steam
 # brew install --cask vlc
