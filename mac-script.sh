4183769080fc7e9c726c0998d77c64eb17388044
Lucas Rodrigues <99984705+lucasrodrigges@users.noreply.github.com>
2024-02-22 04:35:56 -0800

    Update mac-script.sh

--- mac-script.sh
+++ mac-script.sh
@@ -12,9 +12,8 @@ brew install git
 # brew install mysql
 brew install --cask dbeaver-community
 # brew install --cask mysqlworkbench
-# brew install --cask dbeaver-community
 brew install --cask visual-studio-code
-# brew install --cask android-studio
+brew install --cask android-studio
 # brew install --cask sublime-text
 brew install --cask postman
 
@@ -23,26 +22,25 @@ brew install --cask rectangle
 # brew install --cask blackhole-2ch
 brew install --cask android-file-transfer
 
-
-brew install --cask google-chrome
-brew install --cask firefox
+# brew install --cask google-chrome
+# brew install --cask firefox
 brew install --cask whatsapp
-brew install --cask telegram
+# brew install --cask telegram
 brew install --cask discord
 # brew install --cask slack
 # brew install --cask zoom
 brew install --cask hackintool
 brew install --cask obs
-brew install --cask reaper
-brew install --cask google-drive
-brew install --cask qbittorrent
-# brew install --cask notion
+# brew install --cask reaper
+# brew install --cask google-drive
+# brew install --cask qbittorrent
+brew install --cask notion
 # brew install --cask anydesk
 # brew install --cask balenaetcher
 # brew install --cask flameshot
-brew install --cask spotify
+# brew install --cask spotify
 # brew install gimp
-brew install musescore
+# brew install musescore
 # brew install --cask steam
 # brew install --cask vlc
 # brew install --cask geekbench
