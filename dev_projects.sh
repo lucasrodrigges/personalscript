cfb63703489927be01cf5b1c92ca5de500a421fd
lucasrodrigges <rodriggeslucas@gmail.com>
2024-05-13 21:34:59 -0300

    add dev-projects clone

--- dev_projects.sh
+++ dev_projects.sh
@@ -1,11 +1,11 @@
-cd ~/Documents/Benvo
+cd ~/Documents/Benvo &&
 git clone git@github.com:senioramais/benvo-ops.git
 git clone git@github.com:senioramais/benvo-family.git
 git clone git@github.com:senioramais/afarma-frontend.git
 git clone git@github.com:senioramais/afarma-api.git
 git clone git@github.com:senioramais/benvo-documentation.git
 
-cd ~/Documents/BFDB
+cd ~/Documents/BFDB &&
 git clone git@github.com:bfdb-org/bfdb-lp.git
 git clone git@github.com:bfdb-org/bfdb-notifications.git
 git clone git@github.com:bfdb-org/bfdb-tutor.git
